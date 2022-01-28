e3c4dcbfa764abb60652de779c4ea168c5a106fd
Pete Seeling <peteseeling@gmail.com>
2022-01-28 14:45:53 -0600

    animal container finished and correct.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.css';
+
+import { useState } from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [lionSize, setLionSize] = useState(10);
+  const [rhinoSize, setRhinoSize] = useState(10);
+  const [open, setOpen] = useState(['open', 'closed']);
+
+  return (
+    <div className='App'>
+      <div className='animal-container'>
+        <div className='animal-fight'>
+          <img src="lion.jpg" width={lionSize * 10} />
+          <div className='buttons' id='lion'>
+            <button onClick={() => setLionSize(lionSize + 1)}>Lion Bigger</button>
+            <button onClick={() => setRhinoSize(rhinoSize - 1)}>Rhino Smaller</button>
+          </div>
+        </div>
+
+        <div className='animal-fight'>
+          <img src="rhino.jpg" width={rhinoSize * 10} />
+          <div className='buttons'>
+            <button onClick={() => setLionSize(lionSize - 1)}>Lion Smaller</button>
+            <button onClick={() => setRhinoSize(rhinoSize + 1)}>Rhino Bigger</button>
+          </div>
+        </div>
+      </div>
+
+      
+
+
+
+
+    </div>
+    
+  );
+
+}
 export default App;
